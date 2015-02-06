a4792b9895316c7ab79cd36f85fe24a7bf9ef5f6
Alexander Pletzer <pletzer@Tech-Xs-MacBook-Pro.local>
2015-02-06 03:37:06 -0500

    debugging

--- delaunay/pdDelaunay2d.py
+++ delaunay/pdDelaunay2d.py
@@ -45,6 +45,117 @@ class Delaunay2d:
       t[1] = t2
       t[2] = t1
 
+  def _flipEdge(self, bedge):
+
+    tris = self.edge2Triangles.get(bedge)
+
+    # must have two adjacent triangles
+    if tris == None or len(tris) < 2: 
+      return False
+
+    t1, t2 = tris[0], tris[1]
+    triangle1, triangle2 = self.triangles[t1], self.triangles[t2]
+    # opposite nodes i1 and i2
+    i1, i2 = -1, -1
+    for i in range(3):
+      found1, found2 = False, False
+      for j in range(2):
+        found1 |= (bedge[j] == triangle1[i])
+        found2 |= (bedge[j] == triangle2[i])
+      if not found1: i1 = i
+      if not found2: i2 = i
+
+      # get the coordinates of the quadrilateral
+      #     b
+      #    /^\
+      #   / | \
+      #  /  |  \
+      # c 1 | 2 d
+      #  \  |  /
+      #   \ | /
+      #    \|/ 
+      #     a
+      pa, pb = bedge[0], bedge[1]
+      pc, pd = triangle1[i1], triangle2[i2]
+      xya, xyb, xyc, xyd = self.xyPoints[pa], self.xyPoints[pb], self.xyPoints[pc], self.xyPoints[pd]
+
+      # the 2 angles opposite to bedge
+      crossProd1 = 2.0*self._getArea(pc, pa, pb)
+      crossProd2 = 2.0*self._getArea(pd, pb, pa)
+      dotProd1 = (xya[0]-xyc[0])*(xyb[0]-xyc[0]) + (xya[1]-xyc[1])*(xyb[1]-xyc[1])
+      dotProd2 = (xyb[0]-xyd[0])*(xya[0]-xyd[0]) + (xyb[1]-xyd[1])*(xya[1]-xyd[1])
+      angle1 = abs(math.atan2(crossProd1, dotProd1))
+      angle2 = abs(math.atan2(crossProd2, dotProd2))
+   
+      # Delaunay test
+      if angle1 + angle2 > math.pi*(1.0 + self.EPS):
+        # flip
+        #     b
+        #    / \
+        #   /   \
+        #  /  1  \
+        # c-----> d
+        #  \  2  /
+        #   \   /
+        #    \ / 
+        #     a
+        newTriangle1 = [pc, pd, pb]
+        newTriangle2 = [pd, pc, pa]
+        self._makeCounterClockwise(newTriangle1)
+        self._makeCounterClockwise(newTriangle2)
+
+        # reset the triangle list
+        self.triangles[t1] = newTriangle1
+        self.triangles[t2] = newTriangle2
+
+        # create new entry in edge -> triangle map
+        newEdge = (pc, pd)
+        t12 = [t1, t2]
+        self.edge2Triangles[newEdge] = t12
+
+        # update neighboring entries
+        otherEdge = (pb, pd)
+        complOtherEdge = (pd, pb)
+        if self.edge2Triangles.has_key(otherEdge):
+          index = 0
+
+        otherEdge[0] = pb; otherEdge[1] = pd
+        complOtherEdge[0] = pd; complOtherEdge[1] = pb
+        otherEdgeIt = self.edge2Triangles.get(otherEdge, None)
+        complOtherEdgeIt = self.edge2Triangles.get(complOtherEdge, None)
+        if otherEdgeIt != None:
+          index = 0
+          if len(otherEdgeIt) > 1 and otherEdgeIt[1] == t2:
+            index = 1
+          otherEdgeIt[index] = t1
+        elif complOtherEdgeIt != None:
+          index = 0
+          if len(complOtherEdgeIt) > 1 and complOtherEdgeIt[1] == t2:
+            index = 1
+          complOtherEdgeIt[index] = t1
+
+        otherEdge[0] = pc; otherEdge[1] = pa
+        complOtherEdge[0] = pa; complOtherEdge[1] = pc
+        otherEdgeIt = self.edge2Triangles.get(otherEdge, None)
+        complOtherEdgeIt = self.edge2Triangles.get(complOtherEdge, None)
+        if otherEdgeIt != None:
+          index = 0
+          if len(otherEdgeIt) > 1 and otherEdgeIt[1] == t1:
+            index = 1
+          otherEdgeIt[index] = t2
+        elif complOtherEdgeIt != None:
+          index = 0
+          if len(complOtherEdgeIt) > 1 and complOtherEdgeIt[1] == t1:
+            index = 1
+          complOtherEdgeIt[index] = t2
+
+        # remove entry
+        del self.edge2Triangles[bedge]
+
+        return True
+    
+      return False
+
   def _flipAll(self):
     # flip edges until there are no more edges to flip
     flipped = True
@@ -93,9 +204,9 @@ class Delaunay2d:
     e12 = (t[1], t[2])
     e20 = (t[2], t[0])
     self.boundaryEdges += [e01, e12, e20]
-    self.edge2Triangles[e01] = t
-    self.edge2Triangles[e12] = t
-    self.edge2Triangles[e20] = t
+    self.edge2Triangles[e01] = [t,]
+    self.edge2Triangles[e12] = [t,]
+    self.edge2Triangles[e20] = [t,]
 
     # add the remaining points
     for ipoint in range(t[-1], len(self.xyPoints)):
@@ -175,8 +286,8 @@ class Delaunay2d:
         # cache the new boundary edges
         e1 = (newT[0], newT[1])
         e2 = (newT[1], newT[2])
-        newBoundaryEdges.append(e1)
-        newBoundaryEdges.append(e2)
+        newBoundaryEdges.add(e1)
+        newBoundaryEdges.add(e2)
 
         # add the edge -> connectivity entries
         lastTriangleIndex = len(self.triangles) - 1
@@ -187,14 +298,14 @@ class Delaunay2d:
     # remove the old boundary edges
     for i in range(len(edgeIndicesToRemove) - 1, -1, -1):
       ie = edgeIndicesToRemove[i];
-      del self.boundaryEdges[self.boundaryEdges[ie]]
+      del self.boundaryEdges[ie]
 
     # add the new boundary edges (i, j), but only if (j, i) is 
     # not in the list. If both (i, j) and (j, i) are in the list
     # then this is not a boundary edge. 
     for edge in newBoundaryEdges:
       complEdge = (edge[1], edge[0])
-      if newBoundaryEdges.has_key(complEdge):
+      if complEdge in newBoundaryEdges:
         # complEdge was not found so add edge
         self.boundaryEdges.append(edge)
       else:
@@ -205,118 +316,6 @@ class Delaunay2d:
         if e2t is not None:
           e2t.append(complE2t[0])
           del self.edge2Triangles[complEdge]
-
-def _flipEdge(self, bedge):
-
-  tris = self.edge2Triangles.get(bedge)
-
-  # must have two adjacent triangles
-  if tris == None or len(tris) < 2: 
-    return False
-
-  t1, t2 = tris[0], tris[1]
-  triangle1, triangle2 = self.triangles[t1], self.triangles[t2]
-  # opposite nodes i1 and i2
-  i1, i2 = -1, -1
-  for i in range(3):
-    found1, found2 = False, False
-    for j in range(2):
-      found1 |= (bedge[j] == triangle1[i])
-      found2 |= (bedge[j] == triangle2[i])
-    if not found1: i1 = i
-    if not found2: i2 = i
-
-    # get the coordinates of the quadrilateral
-    #     b
-    #    /^\
-    #   / | \
-    #  /  |  \
-    # c 1 | 2 d
-    #  \  |  /
-    #   \ | /
-    #    \|/ 
-    #     a
-    pa, pb = bedge[0], bedge[1]
-    pc, pd = triangle1[i1], triangle2[i2]
-    xya, xyb, xyc, xyd = self.xyPoints[pa], self.xyPoints[pb], self.xyPoints[pc], self.xyPoints[pd]
-
-    # the 2 angles opposite to bedge
-    crossProd1 = 2.0*self._getArea(pc, pa, pb)
-    crossProd2 = 2.0*self._getArea(pd, pb, pa)
-    dotProd1 = (xya[0]-xyc[0])*(xyb[0]-xyc[0]) + (xya[1]-xyc[1])*(xyb[1]-xyc[1])
-    dotProd2 = (xyb[0]-xyd[0])*(xya[0]-xyd[0]) + (xyb[1]-xyd[1])*(xya[1]-xyd[1])
-    angle1 = abs(math.atan2(crossProd1, dotProd1))
-    angle2 = abs(math.atan2(crossProd2, dotProd2))
-   
-    # Delaunay test
-    if angle1 + angle2 > math.pi*(1.0 + self.EPS):
-      # flip
-      #     b
-      #    / \
-      #   /   \
-      #  /  1  \
-      # c-----> d
-      #  \  2  /
-      #   \   /
-      #    \ / 
-      #     a
-      newTriangle1 = [pc, pd, pb]
-      newTriangle2 = [pd, pc, pa]
-      self._makeCounterClockwise(newTriangle1)
-      self._makeCounterClockwise(newTriangle2)
-
-      # reset the triangle list
-      self.triangles[t1] = newTriangle1
-      self.triangles[t2] = newTriangle2
-
-      # create new entry in edge -> triangle map
-      newEdge = (pc, pd)
-      t12 = [t1, t2]
-      self.edge2Triangles[newEdge] = t12
-
-      # update neighboring entries
-      otherEdge = (pb, pd)
-      complOtherEdge = (pd, pb)
-      if self.edge2Triangles.has_key(otherEdge):
-        index = 0
-
-      otherEdge[0] = pb; otherEdge[1] = pd
-      complOtherEdge[0] = pd; complOtherEdge[1] = pb
-      otherEdgeIt = self.edge2Triangles.get(otherEdge, None)
-      complOtherEdgeIt = self.edge2Triangles.get(complOtherEdge, None)
-      if otherEdgeIt != None:
-        index = 0
-        if len(otherEdgeIt) > 1 and otherEdgeIt[1] == t2:
-          index = 1
-        otherEdgeIt[index] = t1
-      elif complOtherEdgeIt != None:
-        index = 0
-        if len(complOtherEdgeIt) > 1 and complOtherEdgeIt[1] == t2:
-          index = 1
-        complOtherEdgeIt[index] = t1
-
-      otherEdge[0] = pc; otherEdge[1] = pa
-      complOtherEdge[0] = pa; complOtherEdge[1] = pc
-      otherEdgeIt = self.edge2Triangles.get(otherEdge, None)
-      complOtherEdgeIt = self.edge2Triangles.get(complOtherEdge, None)
-      if otherEdgeIt != None:
-        index = 0
-        if len(otherEdgeIt) > 1 and otherEdgeIt[1] == t1:
-          index = 1
-        otherEdgeIt[index] = t2
-      elif complOtherEdgeIt != None:
-        index = 0
-        if len(complOtherEdgeIt) > 1 and complOtherEdgeIt[1] == t1:
-          index = 1
-        complOtherEdgeIt[index] = t2
-
-      # remove entry
-      del self.edge2Triangles[bedge]
-
-      return True
-    
-    return False
-
 
 ######################################################
 
