c5178543d72c30e9bf902224a2777427d5567317
Alex Pletzer <axp5530@localhost.localdomain>
2015-04-03 10:43:16 -0400

    updating the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pedees',
-      version='0.1',
+      version='0.2',
       description='A simple PDE solver',
       author='Alexander Pletzer',
       author_email='alexander@gokliya.net',
