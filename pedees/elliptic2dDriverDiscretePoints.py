79b3b1af26bad5e796d3bc99146a14a0a71256d6
ALEXANDER PLETZER <axp5530@psu.edu>
2015-04-08 16:51:45 -0400

    fixing a float/int bug

--- pedees/elliptic2dDriverDiscretePoints.py
+++ pedees/elliptic2dDriverDiscretePoints.py
@@ -9,7 +9,7 @@ class Elliptic2dDriverDiscretePoints(Elliptic2dDriverBase):
 
   def triangulate(self, boundaryPoints, numCells=1000):
       
-    self.boundaryPoints = [ numpy.array(p) for p in boundaryPoints ]
+    self.boundaryPoints = [ numpy.array(p, numpy.float64) for p in boundaryPoints ]
 
     # compute the box dimensions
     xmins =  float('inf') * numpy.ones( (2,), numpy.float64 )
@@ -41,7 +41,7 @@ class Elliptic2dDriverDiscretePoints(Elliptic2dDriverBase):
       du /= float(numSteinerPoints + 1)
       # add the Steiner points
       for i in range(1, numSteinerPoints):
-        pt = self.boundaryPoints[ia] + i * du
+        pt = self.boundaryPoints[ia] + i*du
         self.boundaryPoints.append(pt)
 
     self.delny = Delaunay2d(self.boundaryPoints, maxArea=maxArea)
@@ -51,7 +51,7 @@ class Elliptic2dDriverDiscretePoints(Elliptic2dDriverBase):
     self.delny.makeDelaunay() # this may not be necessary
       
 ################################################################################
-def main():
+def test1():
 
   points = [ [0., 0.], [0., 1.], [1., 1.] ]
   e = Elliptic2dDriverDiscretePoints(fFunc='1.0', gFunc='0.0', sFunc='0.0')
@@ -68,5 +68,24 @@ def main():
   except:
     pass
 
+def test2():
+
+  elliptic = Elliptic2dDriverDiscretePoints(fFunc='1.0', gFunc='0.0', sFunc='0.0')
+
+  bpoints = [numpy.array([0, 0]), numpy.array([0, 1]), numpy.array([1, 1])]
+  elliptic.triangulate(boundaryPoints = bpoints, numCells = 100)
+  elliptic.assemble()
+
+  elliptic.applyBoundaryConditions(bFunc='1.0', cFunc='2.0')
+
+  diag = elliptic.solve()
+
+  try:
+    # requires Tkinter
+    elliptic.show()
+  except:
+    pass
+
 if __name__ == '__main__':
-  main()
+  test1()
+  test2()
